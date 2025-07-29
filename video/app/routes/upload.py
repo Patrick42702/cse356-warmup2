3e787b57965df818291934d15e416f390f61418a
Patrick42702 <patrick42702@yahoo.com>
2025-07-29 11:36:44 -0400

    Fixed upload exception handling and s3 key format

--- video/app/routes/upload.py
+++ video/app/routes/upload.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import boto3
 import magic
 from app.db import db
+from app.s3 import s3
 from app.services.transcode_trigger import trigger_transcode
 from app.util import error, success
 from flask import Blueprint, request
@@ -13,14 +14,6 @@ ALLOWED_EXTENSIONS = {'mp4'}
 ALLOWED_MIME_TYPES = {'video/mp4'}
 
 upload_bp = Blueprint('upload', __name__)
-session = boto3.session.Session()
-
-s3 = session.client(
-    's3',
-    endpoint_url=os.environ.get("S3_ENDPOINT"),
-    aws_access_key_id=os.environ.get("S3_ACCESS_KEY"),
-    aws_secret_access_key=os.environ.get("S3_SECRET_KEY")
-)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -38,29 +31,39 @@ def upload_video():
     video.seek(0)
     if mime not in ALLOWED_MIME_TYPES:
         return error(f"Invalid MIME type: {mime}", 400)
-    video_id = uuid.uuid4()
+    video_id = str(uuid.uuid4())
     filename = f"{video_id}.mp4"
-    s3_key = f"videos/{video_id}/{filename}"
+    s3_key = f"videos/{video_id}"
+    s3_file = f"{s3_key}/{filename}"
     try:
-        s3.upload_fileobj(video, "video-bucket", s3_key)
+        s3.upload_fileobj(video, "video-bucket", s3_file)
     except Exception as e:
         return error(f"S3 Upload failed. {str(e)}", 500)
 
-    url = s3.generate_presigned_url("get_object", {
-        "Bucket": "video-bucket",
-        "Key": s3_key
-    })
+    try:
+        url = s3.generate_presigned_url("get_object", {
+            "Bucket": "video-bucket",
+            "Key": s3_file
+        })
+    except Exception as e:
+        error(f"Error generating S3 URL: {str(e)}", 500)
 
-    db["videos"].insert_one({
-        "video_id": str(video_id),
-        "filename": filename,
-        "uploader_id": "user", # TODO
-        "status": "uploaded",
-        "s3_key": s3_key,
-        "created_at": datetime.now()
-    })
+    try:
+        db["videos"].insert_one({
+            "video_id": video_id,
+            "filename": filename,
+            "uploader_id": "user", # TODO
+            "status": "uploaded",
+            "s3_key": s3_key,
+            "created_at": datetime.now()
+        })
+    except Exception as e:
+        error(f"Error inserting video into the database: {str(e)}", 500)
 
-    trigger_transcode(url, filename, video_id)
+    try:
+        trigger_transcode(url, filename, video_id)
+    except Exception as e:
+        error(f"Transcoding trigger failed: {str(e)}", 500)
 
     return success(data={"url": url}, message="video uploaded")
 
